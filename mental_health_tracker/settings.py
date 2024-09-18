3985d1a5c51d7e295c9eb3ca06e15903b28bf130
mirfaknaufal <mirfakn@gmail.com>
2024-09-18 13:46:27 +0700

    Tutorial 3

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jro83#q12e#)-!=4l&b=aa29q7uhhm*hlwnvubw4-9rf$$dvmb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "mirfak-naufal-mentalhealthtracker.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "mirfak-naufal-mentalhealthtracker.pbp.cs.ui.ac.id", "https://pbp.cs.ui.ac.id/mirfak.naufal/mentalhealthtracker"]
 
 
 # Application definition
@@ -123,4 +125,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://pbp.cs.ui.ac.id/mirfak.naufal/mentalhealthtracker", "https://pbp.cs.ui.ac.id/mirfak.naufal/mentalhealthtracker"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://pbp.cs.ui.ac.id/mirfak.naufal/mentalhealthtracker", "https://pbp.cs.ui.ac.id/mirfak.naufal/mentalhealthtracker","http://mirfak-naufal-mentalhealthtracker.pbp.cs.ui.ac.id/", "https://mirfak-naufal-mentalhealthtracker.pbp.cs.ui.ac.id/"]
